3cf83220a48b6ac356bb9045782c77f3bfead674
Filip Despic <filip.despic@sportserve.co>
2024-09-16 13:39:03 +0200

    add find movie by id

--- src/main/java/com/reactive/client/Movie.java
+++ src/main/java/com/reactive/client/Movie.java
@@ -1,8 +1,10 @@
 package com.reactive.client;
 
 import io.smallrye.mutiny.Multi;
+import io.smallrye.mutiny.Uni;
 import io.vertx.mutiny.pgclient.PgPool;
 import io.vertx.mutiny.sqlclient.Row;
+import io.vertx.mutiny.sqlclient.Tuple;
 
 public class Movie {
 
@@ -22,10 +24,33 @@ public class Movie {
         this.title = title;
     }
 
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
     public static Multi<Movie> findAll(PgPool client) {
         return client.query("SELECT id, title FROM movies ORDER BY title DESC").execute()
                 .onItem().transformToMulti(set -> Multi.createFrom().iterable(set))
                 .onItem().transform(Movie::from);
+    }
+
+    public static Uni<Movie> findById(PgPool client, Long id) {
+        return client.preparedQuery("SELECT id, title FROM movies WHERE id = $1")
+                .execute(Tuple.of(id))
+                .onItem()
+                .transform(m -> m.iterator().hasNext() ? from(m.iterator().next()) : null);
     }
 
     private static Movie from(Row row) {
--- src/main/java/com/reactive/client/MovieResource.java
+++ src/main/java/com/reactive/client/MovieResource.java
@@ -1,11 +1,14 @@
 package com.reactive.client;
 
 import io.smallrye.mutiny.Multi;
+import io.smallrye.mutiny.Uni;
 import io.vertx.mutiny.pgclient.PgPool;
 import jakarta.annotation.PostConstruct;
 import jakarta.inject.Inject;
 import jakarta.ws.rs.GET;
 import jakarta.ws.rs.Path;
+import jakarta.ws.rs.PathParam;
+import jakarta.ws.rs.core.Response;
 
 @Path("movies")
 public class MovieResource {
@@ -21,6 +24,16 @@ public class MovieResource {
     @GET
     public Multi<Movie> get() {
         return Movie.findAll(client);
+    }
+
+    @GET
+    @Path("{id}")
+    public Uni<Response> get(@PathParam("id") Long id) {
+        return Movie.findById(client, id)
+                .onItem()
+                .transform(movie -> movie != null ? Response.ok(movie) : Response.status(Response.Status.NOT_FOUND))
+                .onItem()
+                .transform(Response.ResponseBuilder::build);
     }
 
     private void initdb() {
